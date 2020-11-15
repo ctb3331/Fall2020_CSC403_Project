2aa5ddc98897a5972d74603679919b930608ee2c
ctb3331 <56928584+ctb3331@users.noreply.github.com>
2020-11-14 20:57:07 -0600

    Add files via upload

--- Project/MyGameLibrary/BattleCharacter.cs
+++ Project/MyGameLibrary/BattleCharacter.cs
@@ -7,27 +7,22 @@ using System.Threading.Tasks;
 #pragma warning disable 1591 // use this to disable comment warnings
 
 namespace Fall2020_CSC403_Project.code {
-  public abstract class BattleCharacter : Character {
+  public class BattleCharacter : Character {
     public int Health { get; private set; }
     public int MaxHealth { get; private set; }
     private float strength;
-    private string characterName;
-    public string CharacterName { get => characterName; }
-    private string attackName;
-    public string AttackName{get => attackName; }
 
     public event Action<int> AttackEvent;
 
-    public BattleCharacter(Vector2 initPos, Collider collider, string charName, string charAttackName) : base(initPos, collider) {
-      MaxHealth = 20;
-      strength = 2;
-      Health = MaxHealth;
-      characterName = charName;
-      attackName = charAttackName;
-    }
 
-    //character processing when a character loses in battle depends on the character type (player or enemy)
-    public abstract void HandleLostInBattle();
+
+        public BattleCharacter(Vector2 initPos, Collider collider) : base(initPos, collider) {
+            Random rnd = new Random();
+            strength = rnd.Next(1, 3);
+            MaxHealth = rnd.Next(10, 20);
+
+      Health = MaxHealth;
+    }
 
     public void OnAttack(int amount) {
       AttackEvent((int)(amount * strength));
